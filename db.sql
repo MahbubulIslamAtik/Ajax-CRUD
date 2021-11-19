6f4cef2a84835b0f6f5b5d6d10f4531368600212
MahbubulIslamAtik <atik0790@gmail.com>
2021-11-19 11:44:10 +0600

    Ajax crud

---
+++ db.sql
@@ -0,0 +1,8 @@
+﻿DROP TABLE IF EXISTS `test`.`children`;
+CREATE TABLE  `test`.`children` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `age` double NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
